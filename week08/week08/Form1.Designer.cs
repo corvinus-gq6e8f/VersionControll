cd1a04e2bce7e53af671be6762df33669e1160d0
István Bence Herczegh <istvan.herczegh@stud.uni-corvinus.hu>
2021-11-18 22:06:16 +0100

    szivalaszto gomb

--- week08/week08/Form1.Designer.cs
+++ week08/week08/Form1.Designer.cs
@@ -36,6 +36,7 @@ namespace week08
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mainPanel
@@ -43,9 +44,9 @@ namespace week08
             this.mainPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.mainPanel.Location = new System.Drawing.Point(5, 102);
+            this.mainPanel.Location = new System.Drawing.Point(5, 189);
             this.mainPanel.Name = "mainPanel";
-            this.mainPanel.Size = new System.Drawing.Size(783, 336);
+            this.mainPanel.Size = new System.Drawing.Size(783, 249);
             this.mainPanel.TabIndex = 0;
             // 
             // createTimer
@@ -89,11 +90,22 @@ namespace week08
             this.label1.TabIndex = 3;
             this.label1.Text = "Coming next:";
             // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Lime;
+            this.button3.Location = new System.Drawing.Point(143, 91);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 34);
+            this.button3.TabIndex = 4;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -113,6 +125,7 @@ namespace week08
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
