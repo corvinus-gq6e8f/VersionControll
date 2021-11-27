cec55ffbeae0415c39e4b689fd96c31654a35296
István Bence Herczegh <istvan.herczegh@stud.uni-corvinus.hu>
2021-11-27 21:59:58 +0100

    register happypath and exception

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Activities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -47,6 +48,54 @@ namespace UnitTestExample.Test
 
             //assert
             Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [
+            Test,
+            TestCase("irf@uni-corvinus.hu", "ABCabc12"),
+            TestCase("irf2@uni-corvinus.hu", "Abcd1234")
+        ]
+        public void TestRegisterHappyPath(string email, string password)
+        {
+            //arrange
+            var accountController = new AccountController();
+
+            //act
+            var actualResult = accountController.Register(email, password);
+
+            //assert
+            Assert.AreEqual(email, actualResult.Email);
+            Assert.AreEqual(password, actualResult.Password);
+            Assert.AreNotEqual(Guid.Empty, actualResult.ID);
+        }
+
+        [
+            Test,
+            TestCase("irf@uni-corvinus", "Abcd1234"),
+            TestCase("irf.uni-corvinus.hu", "Abcd1234"),
+            TestCase("irf@uni-corvinus.hu", "abcd1234"),
+            TestCase("irf@uni-corvinus.hu", "ABCD1234"),
+            TestCase("irf@uni-corvinus.hu", "abcdABCD"),
+            TestCase("irf@uni-corvinus.hu", "Ab1234"),
+        ]
+        public void TestRegisterValidateException(string email, string password)
+        {
+            //arrange
+            var accountController = new AccountController();
+
+            //act
+            try
+            {
+                var actualResult = accountController.Register(email, password);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+
+                Assert.IsInstanceOf<ValidationException>(ex);
+            }
+
+            //assert
         }
     }
 }
