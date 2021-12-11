46da6f7f6cf9068ea7f486688eacbd12e2d363b8
István Bence Herczegh <istvan.herczegh@stud.uni-corvinus.hu>
2021-12-11 20:31:15 +0100

    Verseny felépítése

--- whg/whg/Form1.cs
+++ whg/whg/Form1.cs
@@ -62,6 +62,7 @@ namespace whg
             {
                 winnerBrain = winners.FirstOrDefault().Brain.Clone();
                 gc.GameOver -= Gc_GameOver;
+                button1.Visible = true; ;
                 return;
             }
 
@@ -81,6 +82,15 @@ namespace whg
                     gc.AddPlayer(b.Mutate());
             }
             gc.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gc.ResetCurrentLevel();
+            gc.AddPlayer(winnerBrain.Clone());
+            gc.AddPlayer();
+            ga.Focus();
+            gc.Start(true);
         }
     }
 }
