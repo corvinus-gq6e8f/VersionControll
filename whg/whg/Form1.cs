e3cf095c2b3fb83015b173150d7936658fe67128
István Bence Herczegh <istvan.herczegh@stud.uni-corvinus.hu>
2021-12-11 19:55:35 +0100

    Győztes kiválasztása + kommentek

--- whg/whg/Form1.cs
+++ whg/whg/Form1.cs
@@ -19,6 +19,7 @@ namespace whg
         int nbrOfSteps = 10;
         int nbrOfStepsIncrement = 10;
         int generation = 1;
+        Brain winnerBrain = null;
 
         public Form1()
         {
@@ -47,11 +48,24 @@ namespace whg
                 "{0}. generáció",
                 generation);
 
+            //legjobbak
             var playerList = from p in gc.GetCurrentPlayers()
                              orderby p.GetFitness() descending
                              select p;
             var topPerformers = playerList.Take(populationSize / 2).ToList();
 
+            //gyoztesek
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
+
+            //elorol kezdes + legjobbak masolasa
             gc.ResetCurrentLevel();
             foreach (var p in topPerformers)
             {
